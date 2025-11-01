db626e14aec066ef40ab274f87ffb9dfb3105407
akshay-kakade <akshaykakade648@gmail.com>
2025-11-01 21:49:51 +0530

    page update

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const Home = async() => {
    'use cache'
-   cacheLife('seconds')
+   cacheLife('hours')
     const response = await fetch(`${BASE_URL}/api/events`);
     const { events } = await response.json();
   return (
