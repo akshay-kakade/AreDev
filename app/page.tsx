404249ef694805aea291e86deb198e75a7d8383e
akshay-kakade <akshaykakade648@gmail.com>
2025-11-02 11:05:45 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,36 @@
-import EventCard from '@/components/EventCard'
-import ExploreBtn from '@/components/ExploreBtn'
-import { IEvent } from '@/database';
-
+import ExploreBtn from "@/components/ExploreBtn";
+import EventCard from "@/components/EventCard";
+import {IEvent} from "@/database";
+import {cacheLife} from "next/cache";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
-const getEvents = async () => {
-  const response = await fetch(`${BASE_URL}/api/events`, { next: { revalidate: 1 } });
-  const { events } = await response.json();
-  return events;
-}
+const Page = async () => {
+    'use cache';
+    cacheLife('hours')
+    const response = await fetch(`${BASE_URL}/api/events`);
+    const { events } = await response.json();
 
-const Home = async() => {
-  const events = await getEvents();
-  return (
-    
-    <section>
-      <h1 className='text-center'>From Code to Community—Meet, Learn, Grow.</h1>
-      <p className='text-center mt-5'>Hackathons , Meetups, and Conferences, All in One Place</p>
-       <ExploreBtn />
-       <div className='mt-20 space-y-7'>
-        <h3>Featured Events</h3>
+    return (
+        <section>
+            <h1 className="text-center">The Hub for Every Dev <br /> Event You Can&apos;t Miss</h1>
+            <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
 
-        <ul className="events">
+            <ExploreBtn />
+
+            <div className="mt-20 space-y-7">
+                <h3>Featured Events</h3>
+
+                <ul className="events">
                     {events && events.length > 0 && events.map((event: IEvent) => (
                         <li key={event.title} className="list-none">
                             <EventCard {...event} />
                         </li>
                     ))}
                 </ul>
-       </div>
-    </section>
-    
-  )
+            </div>
+        </section>
+    )
 }
 
-export default Home
-
-//03:40:00
+export default Page;
